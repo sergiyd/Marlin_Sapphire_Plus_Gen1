f583055ad4091766f1f53f8abeea76ff8ef211fe
Sergey Dobychin <dobychin@gmail.com>
2021-11-29 23:53:10 +0200

    TFT orientation fix

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2751,7 +2751,7 @@
  *   TFT_ROTATE_270, TFT_ROTATE_270_MIRROR_X, TFT_ROTATE_270_MIRROR_Y,
  *   TFT_MIRROR_X, TFT_MIRROR_Y, TFT_NO_ROTATION
  */
-//#define TFT_ROTATION TFT_NO_ROTATION
+#define TFT_ROTATION TFT_ROTATE_180
 
 //=============================================================================
 //============================  Other Controllers  ============================
